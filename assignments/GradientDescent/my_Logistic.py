dc751df28a5a5e342ff3c119e36e536638cfa6c6
hemanthchebiyam <hc3746@rit.edu>
2023-11-02 22:25:58 -0400

    Assignment 9 code

--- assignments/GradientDescent/my_Logistic.py
+++ assignments/GradientDescent/my_Logistic.py
@@ -3,7 +3,9 @@
 
 import pandas as pd
 import numpy as np
+import pdb
 
+# I did not use the hint file
 class my_Logistic:
 
     def __init__(self, learning_rate=0.1, batch_size=10, max_iter=100, shuffle=False):
@@ -22,53 +24,61 @@ class my_Logistic:
         # X: pd.DataFrame, independent variables
         # y: list, np.array or pd.Series, dependent variables
         data = X.to_numpy()
+        data = np.concatenate([data, np.ones(shape=(data.shape[0], 1))], axis=1)
         d = data.shape[1]
         # Initialize weights as all zeros
         self.w = np.array([0.0] * d)
         self.w0 = 0.0
+        # write your code below
+        for i in range(self.max_iter):
 
-        num_samples = data.shape[0]
-        num_batches = num_samples // self.batch_size
-
-        for _ in range(self.max_iter):
             if self.shuffle:
-                # Shuffle the data if shuffle is True
-                indices = np.random.permutation(num_samples)
-                data = data[indices]
-                y = y[indices]
+                inds = np.random.permutation(range(len(data)))
+            else:
+                inds = np.array(range(len(data)))
 
-            for i in range(num_batches):
-                # Get the current batch
-                start_idx = i * self.batch_size
-                end_idx = (i + 1) * self.batch_size
-                X_batch = data[start_idx:end_idx]
-                y_batch = y[start_idx:end_idx]
+            num_inds = len(inds)
 
-                # Compute predictions
-                predictions = self._sigmoid(np.dot(X_batch, self.w) + self.w0)
+            while num_inds > 0:
 
-                # Compute gradients
-                grad_w = np.dot(X_batch.T, predictions - y_batch) / self.batch_size
-                grad_w0 = np.sum(predictions - y_batch) / self.batch_size
+                num_inds -= self.batch_size
 
-                # Update weights
-                self.w -= self.learning_rate * grad_w
-                self.w0 -= self.learning_rate * grad_w0
+                if num_inds > self.batch_size:
+                    b_inds = inds[:self.batch_size]
+                    inds = inds[self.batch_size:]
 
-    def _sigmoid(self, z):
-        # Sigmoid function
-        return 1 / (1 + np.exp(-z))
+                else:
+                    b_inds = inds
+
+                Xb, yb = data[b_inds], y[b_inds]
+                self.batch_update(Xb, yb)
+
+    def batch_update(self, Xb, yb):
+
+        gradient = grad(wts=self.w, Xb=Xb, yb=yb)
+        self.w -= self.learning_rate * gradient
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables
-        # prob is a dict of prediction probabilities belonging to each category
-        # return probs = f(x) = 1 / (1+exp(-(w0+w*x))); a list of float values in [0.0, 1.0]
-        data = X.to_numpy()
-        return self._sigmoid(np.dot(data, self.w) + self.w0)
+        # prob is a dict of prediction probabilities belonging to each categories
+        # return probs = f(x) = 1 / (1+exp(-(w0+w*x))}); a list of float values in [0.0, 1.0]
+        # write your code below
+        X = np.concatenate([X, np.ones(shape=(X.shape[0], 1))], axis=1)
+        probs = sigmoid(X @ self.w)
+        return probs
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, str
         # return predictions: list of int values in {0, 1}
+
         probs = self.predict_proba(X)
         predictions = [1 if prob >= 0.5 else 0 for prob in probs]
         return predictions
+
+def grad(wts, Xb, yb):
+    m = len(yb)
+
+    return 2 * (1 / m) * Xb.T @ ((sigmoid(Xb @ wts) - yb) * sigmoid(Xb @ wts) * (1 - sigmoid(Xb @ wts)))
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
